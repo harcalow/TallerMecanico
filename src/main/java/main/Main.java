efede1af3e7f6f187bb15a1c1723c031c2e490b4
Harold Calderon <harold.1408@hotmail.com>
2021-09-27 02:15:58 -0500

    Se implementa vistas

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -24,7 +24,11 @@ public class Main {
     private static final String SELECT_ALL_FROM_EMPLEADO_SOLEDAD = String.format("SELECT * FROM empleadosSoledadView", DATA_BASE_NAME);
     private static final String SELECT_ALL_FROM_EMPLEADO_BUCARAMANGA= String.format("SELECT * FROM empleadosBucaramagaView", DATA_BASE_NAME);
     private static final String SELECT_ALL_FROM_EMPLEADO_BELLO = String.format("SELECT * FROM empleadosBelloView", DATA_BASE_NAME);
-
+    private static final String CALL_SP_INSERTAR_PROPIETARIO = String.format("CALL sp_insertarPropietario(1030656516,1,'Harold','Yesid','Calderon','Vega','cll','4','A','51',null,'harold.1408@hotmail.com','9000940','1995-08-14')", DATA_BASE_NAME);
+    private static final String CALL_SP_Borrar_PROPIETARIO = String.format("CALL sp_eliminarPropietario()", DATA_BASE_NAME);
+    private static final String CALL_SP_PROPIETARIOS = String.format("CALL sp_propietarios()", DATA_BASE_NAME);
+    private static final String CALL_SP_CONSULTAR_REVISIONES = String.format("CALL sp_colsultarRevisiones()", DATA_BASE_NAME);
+    private static final String CALL_SP_ACTUALIZAR_HORA = String.format("sp_actualizarHoraRevision(3,'7:00:00')", DATA_BASE_NAME);
 
 
     /*private static final String SELECT_ALL_FROM_CIUDAD = String.format("select * from %s.ciudad", DATA_BASE_NAME);
@@ -40,11 +44,12 @@ public class Main {
         while (true){
             System.out.println("Que deseas hacer");
             System.out.println("1) Verificar Empleados por ciudades");
-            System.out.println("2) Insertar propietario");
-            System.out.println("3) Listar propietarios");
-            System.out.println("4) Consultar revisiones en preoceso");
-            System.out.println("5) Borrar marca de automovil");
-            System.out.println("6) Actualizar hora de cita para revision");
+            System.out.println("2) Insertar propietario predeterminado");
+            System.out.println("3) Borrar propietario predeterminado");
+            System.out.println("4) Listar propietarios");
+            System.out.println("5) Consultar revisiones en proceso");
+            System.out.println("6) Actualizar hora de cita para revision predeterminada");
+            System.out.println("7) Salir");
 
             entrada=scan.nextLine();
             if (entrada.equals("1")){
@@ -100,12 +105,46 @@ public class Main {
                         selectAllEMPLEADOBELLO();
                         logout();
                     }else {
-                        System.out.println("No existe");
+                        System.out.println("No existe opcion");
                     }
                     System.out.println("*********************************************");
             }
             else if (entrada.equals("2")){
+                login();
+                call_sp_insertarPropietario();
+                logout();
+                System.out.println("Insertado");
+                System.out.println("*********************************************");
+            }
+            else if (entrada.equals("3")) {
+                login();
+                call_sp_borrarPropietario();
+                logout();
+                System.out.println("Borrado");
+                System.out.println("*********************************************");
+            }
+            else if (entrada.equals("4")){
+                login();
+                call_sp_listarPropietario();
+                logout();
+                System.out.println("*********************************************");
+            }
+            else if (entrada.equals("5")){
+                login();
+                call_sp_consultarRevisiones();
+                logout();
+                System.out.println("*********************************************");
+            }
+            else if (entrada.equals("6")){
+                login();
+                call_sp_actualizarHora();
+                logout();
+                System.out.println("*********************************************");
+            }else if (entrada.equals("7")) {
                 break;
+            }
+            else{
+                System.out.println("No existe opcion");
             }
         }
     }
@@ -168,6 +207,31 @@ public class Main {
         mySqlOperations.setSqlStatement(SELECT_ALL_FROM_EMPLEADO_BELLO);
         mySqlOperations.executeSqlStatement();
         mySqlOperations.printResultSet();
+    }
+    private static void call_sp_insertarPropietario() throws SQLException {
+        mySqlOperations.setSqlStatement(CALL_SP_INSERTAR_PROPIETARIO);
+        mySqlOperations.executeSqlStatement();
+        mySqlOperations.printResultSet();
+    }
+    private static void call_sp_borrarPropietario() throws SQLException {
+        mySqlOperations.setSqlStatement(CALL_SP_Borrar_PROPIETARIO);
+        mySqlOperations.executeSqlStatement();
+        mySqlOperations.printResultSet();
+    }
+    private static void call_sp_listarPropietario() throws SQLException {
+        mySqlOperations.setSqlStatement(CALL_SP_PROPIETARIOS);
+        mySqlOperations.executeSqlStatement();
+        mySqlOperations.printResultSet();
+    }
+    private static void call_sp_consultarRevisiones() throws SQLException {
+        mySqlOperations.setSqlStatement(CALL_SP_CONSULTAR_REVISIONES);
+        mySqlOperations.executeSqlStatement();
+        mySqlOperations.printResultSet();
+    }
+    private static void call_sp_actualizarHora() throws SQLException {
+        mySqlOperations.setSqlStatement(CALL_SP_ACTUALIZAR_HORA);
+        mySqlOperations.executeSqlStatement();
+        //mySqlOperations.printResultSet();
     }
 
 
